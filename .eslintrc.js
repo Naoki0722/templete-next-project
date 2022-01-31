d3bc411963f5ae0de8012354b45b7372a3078b3c
Naoki0722 <mattu.nao722@gmail.com>
2022-02-01 01:21:40 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['next', 'next/core-web-vitals', 'prettier'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'newline-before-return': 'error',
+    'no-console': 'warn',
+    'no-var': 'error',
+  },
+}
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true, // シングルコーテーションを使う
+  jsxSingleQuote: true, // JSX もシングルコーテーションに
+  arrowParens: 'always', // Arrow 関数の引数に () を付ける
+  // endOfLine: 'lf', // 改行コード LF
+}
